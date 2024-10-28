2550f05154d78fb3261dd6423ae78dce15df4d64
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-27 22:35:07 -0400

    Implement shape drawing logic for lines, rectangles, and circles.

--- app.js
+++ app.js
@@ -28,3 +28,22 @@ canvas.addEventListener('mouseup', (event) =>{ // stops drawing mouse up
     const endY = event.offsetY;
     drawShape(selectedShape, startX, startY, endX, endY); //draw shape
 });
+
+//Task 3: Implement Shape Drawing Logic
+function drawShape(shape, x1, y1, x2, y2){
+    context.beginpath();
+    context.strokeStyle= colorPicker.value;
+
+    if(shape === 'line'){ //line 
+        context.moveTo (x1,y1);
+        context.lineTo (x2,y2);
+    } else if (shape === 'rectangle'){ //rectangle
+        const width = x2 - x1;
+        const height = y2 - y1;
+        context.rect(x1, y1, width, height);
+    } else if (shape === 'circle'){ // circle
+        const radius = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+        context.arc(x1, y1, radius, 0, 2 * Math.PI);
+    }
+    context.stroke();
+}
